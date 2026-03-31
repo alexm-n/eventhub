ff4fd401d5b2759a16c856afc5e073ed442550a9
alex <alex@alex-xps>
2026-03-31 13:59:40 +0200

    adding views and urls

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Event, Participant, Registration
+from .serializers import EventSerializer, ParticipantSerializer, RegistrationSerializer
 
-# Create your views here.
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class ParticipantViewSet(viewsets.ModelViewSet):
+    queryset = Participant.objects.all()
+    serializer_class = ParticipantSerializer
+
+class RegistrationViewSet(viewsets.ModelViewSet):
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializer
