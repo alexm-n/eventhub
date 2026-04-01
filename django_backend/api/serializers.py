b3e529bdb3fa16cd9b5ad31229f2c1664e2602e8
Dardeos <87314938+Dardeos@users.noreply.github.com>
2026-04-01 20:21:16 +0200

    Add custom token serializer with user role claims
    Added CustomTokenObtainPairSerializer to include user role in token.

--- django_backend/api/serializers.py
+++ django_backend/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import Event, Participant, Registration
 
 class EventSerializer(serializers.ModelSerializer):
@@ -15,3 +16,13 @@ class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Registration
         fields = '__all__'
+
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['role'] = 'admin' if user.is_staff else 'viewer'
+        return token
