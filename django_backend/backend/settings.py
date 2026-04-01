923273b29937f5e38a707e2d02034a00f43a1e60
Dardeos <87314938+Dardeos@users.noreply.github.com>
2026-04-01 20:18:20 +0200

    Update Django settings for backend project

--- django_backend/backend/settings.py
+++ django_backend/backend/settings.py
@@ -130,7 +130,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
-
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    "http://localhost:5173",#pour vite
+    "http://localhost:3000",
 ]
+
