f68dcb38a01c0707d11cbe24515ef9fd4aa563cc
Dardeos <87314938+Dardeos@users.noreply.github.com>
2026-04-04 15:29:00 +0200

    Update urls.py

--- django_backend/backend/urls.py
+++ django_backend/backend/urls.py
@@ -12,6 +12,6 @@ router.register(r'registrations', RegistrationViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
